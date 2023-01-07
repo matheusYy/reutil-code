2b314ad3ca02574ec813bc0f860bf8eb1c222207
not_matheusyy <matheusrodrigues.yy7@gmail.com>
2023-01-07 03:21:42 +0000

    Update 2 files

--- reutil-code/src/util/forms/formik.tsx
+++ reutil-code/src/util/forms/formik.tsx
@@ -1,55 +1,51 @@
-import { useFormik } from 'formik';
+import React from "react";
+import { useFormik } from "formik";
+const validate = (values) => {
+  const errors = {};
 
+  if (!values.firtName) {
+    errors = "error";
+} if else (values.firtName.length < 1) {
+    errors = 'coloque algo...'
+};
+  return errors;
+};
 
-const validate = values => {
- const error = {};
-  if(!values.nome) {
-      erros.nome = 'é preciso colocar o seu nome!'
-  } else if (values.nome.length > 14) {
-      erros.nome = 'coloque um nome mais curto...'
-  };
-  if(!values.email) {
-      error.email = 'é preciso colocar o seu email!'
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      error.email = 'formato de email invalido!'
-  }
-  
-  return error;
-}:
-
+console.log(validate);
 export const InputFormik = (): TSX.Element => {
+  const formik = useFormik({
+    initialValues: {
+      firtName: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 1)); //
+    },
+  });
+  console.log(formik.errors.firtName);
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="nome">nome</label>
+      <input
+        id="firtName"
+        name="firtName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firtName}
+      />
+      {formik.errors.firtName ? <div>error</div> : null}
+      <label htmlFor="email">email</label>
+      <input
+        name="user_email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
 
-const formik = useFormik({
- initialValues: {
-  nome: '',
-  email: '',
- },
- validate,
- onSubmit: values => {
-  alert(JSON.stringify(values, null, 1));  // 
- }, 
-});
-
-console.log(formik.values.email)
- return (
-  <form onSubmit={formik.handleSubmit} > 
-    <label htmlFor="nome">nome</label>
-    <input 
-     id="nome" 
-     name="nome" 
-     type="text" 
-     onChange={formik.handleChange} 
-     value={formik.values.nome} />
-   
-    <label htmlFor="email">email</label>
-    <input 
-     id="email"  
-     name="email" 
-     type="email" 
-     onChange={formik.handleChange} 
-     value={formik.values.email} />
-     
-     <button type="submit" className="btn-pdr">enviar</button>
-  </form>       
- )
-}
+      <button type="submit" className="btn-pdr">
+        enviar
+      </button>
+    </form>
+  );
+};
