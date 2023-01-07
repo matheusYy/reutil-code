316313d7f64723f3858190203838f2b952b95af1
not_matheusyy <matheusrodrigues.yy7@gmail.com>
2023-01-07 23:16:14 +0000

    Update 14 files

--- reutil-code/src/util/forms/formik.tsx
+++ reutil-code/src/util/forms/formik.tsx
@@ -1,51 +1,87 @@
-import React from "react";
-import { useFormik } from "formik";
-const validate = (values) => {
-  const errors = {};
+//import methods
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useState } from "react";
+//style
+import "./scss/formik.css";
+//animation itens
+import { Checkted, Reload } from "./icones_animation/itens";
+//create object in Yup
+const SignupSign = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "muito curto...")
+    .max(45, "limite de caracteres...")
+    .required("é preciso preencher esse campo"),
+  email: Yup.string().email("email invalido").required("coloque o seu email!"),
+});
 
-  if (!values.firtName) {
-    errors = "error";
-} if else (values.firtName.length < 1) {
-    errors = 'coloque algo...'
-};
-  return errors;
-};
+interface formsValues {
+  name: string;
+  email: string;
+}
+export const InputFormik: React.FC = () => {
+  const [reload, setReload] = useState(false);
+  const initialValues: formsValues = { name: "", email: "" };
+  const sleep = (ms: any) => new Promise((r) => setInterval(r, ms));
 
-console.log(validate);
-export const InputFormik = (): TSX.Element => {
-  const formik = useFormik({
-    initialValues: {
-      firtName: "",
-      email: "",
-    },
-    validate,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 1)); //
-    },
-  });
-  console.log(formik.errors.firtName);
+  async function animate() {
+    try {
+      await setReload(true);
+      await setTimeout(() => {
+        window.alert("ALGO");
+        setReload(false);
+      }, 800);
+    } catch (e) {
+      return e;
+    }
+  }
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="nome">nome</label>
-      <input
-        id="firtName"
-        name="firtName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.firtName}
-      />
-      {formik.errors.firtName ? <div>error</div> : null}
-      <label htmlFor="email">email</label>
-      <input
-        name="user_email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-
-      <button type="submit" className="btn-pdr">
-        enviar
-      </button>
-    </form>
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={SignupSign}
+        onSubmit={async (value, action) => {
+          try {
+            await animate();
+            action.setSubmitting(true);
+            window.alert(action.setSubmitting);
+          } catch (a) {
+            return a;
+          }
+        }}
+      >
+        {({ isValid, dirty, handleSubmit }) => (
+          <Form
+            autoComplete="on"
+            noValidate
+            action="https://formsubmit.co/matheusrodrigues.yy7@gmail.com"
+            method="post"
+            onSubmit={() => animate()}
+          >
+            <label htmlFor="nome">nome</label>
+            <Field id={"nome"} name={"name"} placeholder={"seu nome..."} />
+            <ErrorMessage name={"name"} />
+            <label htmlFor="email">email</label>
+            <Field
+              id={"email"}
+              name="email"
+              type="email"
+              placeholder={"seu email..."}
+            />
+            <ErrorMessage name={"email"} />
+            <button
+              type="submit"
+              className="submit-btn-interfac"
+              disabled={!isValid || !dirty}
+            >
+              enviar
+              {reload == true ? <Reload /> : <Checkted />}
+            </button>
+            <input type="hidden" name="_next" value="" />
+            <input type="hidden" name="_template" value="table" />
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
